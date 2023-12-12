952be9ea8ac6ecac78ec123a4bfca9e86045c489
Pratyaksh <pratyaksh@exitfund.com>
2023-12-12 23:12:50 +0530

    Added New Service: Shopping Cart Service

--- backend/api-gateway/routes.js
+++ backend/api-gateway/routes.js
@@ -28,19 +28,22 @@ const ROUTES = [
                 ['^/']: '',
             }
         }
+    },
+    {
+        url: '/carts',
+        auth: false,
+         // rateLimit: {
+        //     windowMs: 15 * 60 * 1000,
+        //     max: 10
+        // },
+        proxy: {
+            target: "http://localhost:3003",
+            changeOrigin: true,
+            pathRewrite: {
+                ['^/']: '',
+            }
+        }
     }
-    // {
-    //     url: '/premium',
-    //     auth: true,
-    //     creditCheck: true,
-    //     proxy: {
-    //         target: "https://www.google.com",
-    //         changeOrigin: true,
-    //         pathRewrite: {
-    //             [`^/premium`]: '',
-    //         },
-    //     }
-    // }
 ]
 
 exports.ROUTES = ROUTES;
