2a0062da8ac407a9e235d5e04bc5ef71d7d05d1f
Pratyaksh <pratyaksh@exitfund.com>
2023-12-19 23:30:04 +0530

    Order Service Apis done

--- backend/orderService/controllers/order.controller.js
+++ backend/orderService/controllers/order.controller.js
@@ -99,3 +99,83 @@ exports.userOrders = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.updateOrder = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    const updatedOrderDetails = req.body;
+
+    if (!orderId)
+      return res
+        .status(400)
+        .json({ status: "error", message: "Order ID is required." });
+    if (!updatedOrderDetails || Object.keys(updatedOrderDetails).length === 0) {
+      return res.status(400).json({
+        status: "error",
+        message: "Updated order details are required.",
+      });
+    }
+
+    const updatedOrder = await Order.findByIdAndUpdate(
+      orderId,
+      updatedOrderDetails,
+      {
+        new: true, // Return the modified document rather than the original
+      }
+    );
+
+    if (!updatedOrder)
+      return res
+        .status(404)
+        .json({ status: "error", message: "Order not found." });
+    return res.status(200).json({
+      status: "ok",
+      message: "Order updated successfully.",
+      data: updatedOrder,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+exports.deleteOrder = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+
+    if (!orderId)
+      return res
+        .status(400)
+        .json({ status: "error", message: "Order ID is required." });
+
+    const deletedOrder = await Order.findByIdAndDelete(orderId);
+
+    if (!deletedOrder)
+      return res
+        .status(404)
+        .json({ status: "error", message: "Order not found." });
+
+    return res.status(200).json({
+      status: "ok",
+      message: "Order deleted successfully.",
+      data: deletedOrder
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+
+exports.ordersByStatus = async(req, res, next) => {
+  try {
+    const {status} = req.params;
+    if (!status) return res.status(400).json({ status: "error", message: "Status is required." });
+    const ordersByStatus = await Order.find({ status });
+    if (ordersByStatus.length < 1) return res.status(400).json({ status: "error", message: "Orders not found.", data: [] });
+    return res.status(200).json({ status: 'ok', message: 'Data found.', data: ordersByStatus});
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
--- backend/orderService/routes/order.route.js
+++ backend/orderService/routes/order.route.js
@@ -6,5 +6,8 @@ router.get("/ping", orderController.Ping)
 router.post('/:userId', orderController.createOrder)
 router.get('/:orderId', orderController.orderById);
 router.get('/user/:userId', orderController.userOrders);
+router.put('/:orderId', orderController.updateOrder)
+router.delete('/:orderId', orderController.deleteOrder)
+router.get('/status/:status', orderController.ordersByStatus)
 
 module.exports = router;
