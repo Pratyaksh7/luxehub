62886c0d98574a697fe8420b7aed682284b566dd
Pratyaksh Gupta <pratyaksh@exitfund.com>
2025-05-11 13:37:51 +0530

    cart service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [AuthModule, ProductsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
